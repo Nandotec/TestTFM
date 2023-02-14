4c19463c42c527c9844ae3fe11a0ad84412ef5a2
Juan Fernando Ulloa Morán <juan.fernando.ulloa@gmail.com>
2023-02-13 19:54:40 -0600

    Update app.py

--- TFM/app.py
+++ TFM/app.py
@@ -5,11 +5,11 @@ app.secret_key = "JFUJLUMCS_111"
 
 @app.route("/hello")
 def index():
-	flash("What's your name?")
+	#flash("What's your name?")
 	return render_template("index.html")
 
 @app.route("/greet", methods=["POST", "GET"])
 def greet():
-	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
-	request.form['name_input']
+	#flash("Hi " + str(request.form['name_input']) + ", great to see you!")
+	#request.form['name_input']
 	return render_template("index.html")
